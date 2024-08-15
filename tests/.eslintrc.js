bf230b6c436eeaf11c54abcd63b17bfb9d1acad2
guido <dev@guidoscialfa.com>
2024-08-15 18:40:21 +0200

    Fix testing QA

--- tests/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  extends: path.resolve(__dirname, '../.eslintrc.json'),
-  parserOptions: {
-    project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
-  },
-};
