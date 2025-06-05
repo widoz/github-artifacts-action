80d2d5bf83ef56d42e9ce3577f9d79fdfa5df264
Guido Scialfa <dev@guidoscialfa.com>
2025-06-05 19:16:04 +0200

    Just avoid tags

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core';
 import { Artifacts } from '@model/artifacts';
-import { Tags } from '@model/tags';
-import { TemporaryBranch } from '@model/temporary-branch';
+// import { Tags } from '@model/tags';
+// import { TemporaryBranch } from '@model/temporary-branch';
 import { createGit } from './create-git';
 import { Configuration } from './configuration';
 
@@ -9,14 +9,13 @@ async function main(): Promise<void> {
   const configuration = new Configuration(core.getInput.bind(core));
 
   const git = await createGit();
-  const tags = new Tags(git);
-  const artifacts = new Artifacts(git, tags, configuration);
-  const temporaryBranch = new TemporaryBranch(git);
+  const artifacts = new Artifacts(git, /*tags,*/ configuration);
+  // const temporaryBranch = new TemporaryBranch(git);
 
   Promise.resolve()
-    .then(() => temporaryBranch.create())
+    // .then(() => temporaryBranch.create())
     .then(() => artifacts.update())
-    .then(() => temporaryBranch.delete())
+    // .then(() => temporaryBranch.delete())
 
     .catch((error) => {
       core.setFailed(`Failed to create and push artifacts: ${error}`);
